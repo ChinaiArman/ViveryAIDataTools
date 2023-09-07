eece6e8a6b3b0fefa08d6ca27a9efb6b3569db49
ChinaiArman <chinaiarman@gmail.com>
2023-09-07 11:59:22 -0700

    Create hours_clean_script.py

---
+++ hours_clean_script.py
@@ -0,0 +1,22 @@
+import os
+import openai
+
+from keys import API_KEY
+
+openai.api_type = "azure"
+openai.api_base = "https://viveryadvocate.openai.azure.com/"
+openai.api_version = "2022-12-01"
+openai.api_key = API_KEY
+
+response = openai.Completion.create(
+  engine="arman_hours_clean_model",
+  prompt="Q: Mon-Fri,10:00:00 AM,4:00:00 PM\nA:",
+  temperature=0.2,
+  max_tokens=100,
+  top_p=1,
+  frequency_penalty=0,
+  presence_penalty=0,
+  best_of=1,
+  stop=["%%"])
+
+print(response)
