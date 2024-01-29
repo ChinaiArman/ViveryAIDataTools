f4fa3ccabcf0417183ff0d4d73f11d3cde034c56
ChinaiArman <chinaiarman@gmail.com>
2024-01-28 17:43:40 -0800

    Create module docs and imports

---
+++ identify_primary_contact.py
@@ -0,0 +1,20 @@
+"""
+Clean Hours Script
+
+@author Arman Chinai
+@version 1.0.1
+"""
+
+# PACKAGE IMPORTS
+import openai
+import argparse, os, shutil
+import pandas as pd
+
+# LOCAL FILE IMPORTS
+
+
+# AI CONSTANTS
+from keys import CLEAN_HOURS_KEY as OAI_API
+
+# MISC CONSTANTS
+PROMPT = """"""
