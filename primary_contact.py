f068fcebc830300dc565a88aae9980ac2d41b9fb
ChinaiArman <chinaiarman@gmail.com>
2024-01-28 17:59:29 -0800

    Create if main structure

--- primary_contact.py
+++ primary_contact.py
@@ -18,3 +18,48 @@ from keys import CLEAN_HOURS_KEY as OAI_API
 
 # MISC CONSTANTS
 PROMPT = """"""
+
+
+
+
+# HELPERS
+def list_of_strings(arg: str) -> list:
+    """
+    """
+    return arg.split(',')
+
+
+def create_id_contacts_dict(df: pd.DataFrame) -> dict:
+    """
+    """
+    pass
+
+
+
+
+# TESTS
+
+
+
+
+# MAIN
+if __name__ == '__main__':
+    # Define console parser
+    parser = argparse.ArgumentParser(description="Identify the primary contacts from a bulk upload file")
+    # Add file argument
+    parser.add_argument("file", action="store", help="A bulk upload file")
+    # Add primary key argument
+    parser.add_argument("pk", action="store", help="A unique key to identify each row")
+    # Add column arguments
+    parser.add_argument("--columns", type=list_of_strings, action="store", help="The columns that contain contact information")
+    # Console arguments
+    args = parser.parse_args()
+
+    # Create DataFrame
+    df = pd.read_csv(args.file)
+    # Move CSV
+    shutil.move(args.file, "csvs/" + args.file.replace("csvs\\", ""))
+    # Create id_hours Dictionary
+    id_hours_dict = create_id_contacts_dict(df)
+    # Create is_valid_hours Dictionary
+    is_valid_hours_dict = {key: True for key, _ in id_hours_dict.items()}
