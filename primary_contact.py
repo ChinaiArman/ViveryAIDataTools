3a84df909aa4ff2a37741f77ca5dea68fae0ddbf
ChinaiArman <chinaiarman@gmail.com>
2024-02-04 00:26:48 -0800

    Separate prompt into individual prompt for each contact piece

--- primary_contact.py
+++ primary_contact.py
@@ -18,36 +18,57 @@ import time
 from keys import PRIMARY_CONTACT_KEY as OAI_API
 
 # MISC CONSTANTS
-PROMPT = """
-Given an input string, create the following output dictionary:
-
-"
-{
-    "P#": [phone_number],
-    "P#Ext": [phone_number_extension],
-    "Email": [email],
-    "Name": [name],
-    "Marker": [marker]
-}
-"
-
-Keys:
-P# will contain a phone number if present, else None. Phone numbers must be stored using the following format, with X representing the phone number digits: XXX-XXX-XXXX
-P#Ext will contain a phone number extension if present, else None. 
-Email will contain an email address if present, else None.
-Name will contain a name if present, else None.
-Marker will be a boolean value (True/False). Marker is True if there are multiple values present for any of the above keys, or if the value for all keys is None. Otherwise, marker is False.
-
-Formatting Notes:
-Do not include information that does not fall into one of the above keys.
-Do not use any tokens from outside of the input string.
-Use None for any values that are not present in the input string.
-!!! Do NOT attempt to generation information to fill the keys, if no phone number is present, do not include a phone number, only None for missing values.
-!!! ALL Keys must be present in the dictionary, even if their value is None.
-!!! The value for Name must be formatted in FirstName LastName, else None.
+PROMPTS = {
+"Number": 
 """
+Extract the phone from the following string, and append "%%" DIRECTLY after the number. 
 
+!!! Format the number using the following format: "XXX-XXX-XXXX%%", with "X" representing a digit 0-9.
+!!! If there is no phone number present in the following text, return "NA" followed by "%%"
 
+Input: "(513)-153-5915, johnnyappleseed@gmail.com"
+Output: 513-153-5915%%
+""",
+"Email": 
+"""
+Extract the email from the following string, and append "%%" DIRECTLY after the email domain. 
+
+!!! Format the email using the following format: "[prefix]@[domain].[extension]%%".
+!!! If there is NO EMAIL PRESENT in the following text, return "NA" followed by "%%"
+
+Input: "513-153-5915, johnnyappleseed@gmail.com"
+Output: johnnyappleseed@gmail.com%%
+
+Input: "joannepeach53@gmail.com"
+Output: joannepeach53@gmail.com%%
+""",
+"Name": 
+"""
+Extract the first and last name of this persons from their contact information in the following string, and append "%%" DIRECTLY after the number. 
+
+!!! Do NOT use numbers or symbols in the output.
+!!! First and Last name must start with a capital letter.
+!!! The output must contain two words, and have a space separating them.
+!!! If there is no name present in the following text, return "NA" followed by "%%".
+
+Input: "513-153-5915, johnnyappleseed@gmail.com"
+Output: Johnny Appleseed%%
+""",
+"Extension": 
+"""
+Extract the phone extension from the following string, and append "%%" DIRECTLY after the number. 
+
+!!! Format the extension using the following format: "XXXX%%", with "X" representing a digit 0-9. The number of digits depends on the length of the phone extension.
+!!! If there is no extension present in the following text, return "NA" followed by "%%"
+!!! DO NOT INCLUDE NUMBERS THAT ARE APART OF EMAIL ADDRESSES OR THE BASE PHONE NUMBER, ONLY NUMBERS CLEARLY MARKED AS EXTENSIONS.
+
+Input: "513-153-5915 EXT 5315, johnnyappleseed19845@gmail.com"
+Output: 5315%%
+
+Input: "158-159-0915, joannepeach53@gmail.com Joanne Peach"
+Output: NA%%
+"""
+}
 
 
 # HELPERS
@@ -66,28 +87,27 @@ def create_id_contacts_dict(df: pd.DataFrame, primary_key: str, contact_columns:
     return id_contacts_dict
 
 
-def call_oai(prompt: str) -> str:
+def call_oai(prompt: str, case: str) -> str:
     """
     """
-    print(f"IN: {prompt}")
     openai.api_type = "azure"
     openai.api_base = OAI_API["base"]
     openai.api_version = "2023-09-15-preview"
     openai.api_key = OAI_API["key"]
     response = openai.Completion.create(
         engine=OAI_API["engine"],
-        prompt=f"{PROMPT}\Input String: {prompt}\nOutput Dictionary: ",
+        prompt=f'{prompt}\nInput: "{case}"\nOutput: ',
         temperature=0.4,
         max_tokens=50,
         top_p=0.25,
         frequency_penalty=0,
         presence_penalty=0,
-        best_of=2,
-        stop=["}"]
+        best_of=1,
+        stop=["%%"]
     )
     time.sleep(0.05)
     print("\tOAI API Response: " + response["choices"][0]["text"])
-    return response["choices"][0]["text"] + "}"
+    return response["choices"][0]["text"]
 
 
 def format_contacts_iteratively(id_contacts_dict: dict) -> dict:
@@ -95,12 +115,7 @@ def format_contacts_iteratively(id_contacts_dict: dict) -> dict:
     """
     primary_contacts_dict = {}
     for key, value in id_contacts_dict.items():
-        new_value = call_oai(value)
-        try:
-            new_value = eval(new_value.replace("\n", ""))
-        except:
-            pass
-        primary_contacts_dict[key] = new_value
+        primary_contacts_dict[key] = {prompt_type: call_oai(prompt, value) for prompt_type, prompt in PROMPTS.items()}
     return primary_contacts_dict
 
 
@@ -128,41 +143,29 @@ def convert_id_hours_dict_to_df(valid_id_contacts_dict: dict, is_valid_contact_d
 def test_name_in_original_string(id_contacts_dict: dict, primary_contacts_dict: dict, is_valid_contact_dict: dict) -> dict:
     """
     """
-    for key, value in primary_contacts_dict.items():
-        is_valid = True
-        try:
-            for name in value["Name"].split(" "):
-                is_valid = name.lower().replace(" ", "") in id_contacts_dict[key].lower().replace(" ", "") and is_valid
-        except: 
-            is_valid = True
-        is_valid_contact_dict[key] = is_valid_contact_dict[key] and is_valid
+    # for key, value in primary_contacts_dict.items():
+    #     is_valid = True
+    #     try:
+    #         for name in value["Name"].split(" "):
+    #             is_valid = name.lower().replace(" ", "") in id_contacts_dict[key].lower().replace(" ", "") and is_valid
+    #     except: 
+    #         is_valid = True
+    #     is_valid_contact_dict[key] = is_valid_contact_dict[key] and is_valid
     return is_valid_contact_dict
 
 
 def test_name_format(_: dict, primary_contacts_dict: dict, is_valid_contact_dict: dict) -> dict:
     """
     """
-    for key, value in primary_contacts_dict.items():
-        is_valid = True
-        try:
-            is_valid = value["Name"].replace(" ", "").isalpha() and is_valid
-            for name in value["Name"].split(" "):
-                is_valid = name.replace(" ", "")[0].isupper() and is_valid
-        except: 
-            is_valid = True
-        is_valid_contact_dict[key] = is_valid_contact_dict[key] and is_valid
-    return is_valid_contact_dict
-
-
-def test_is_dictionary(_: dict, primary_contacts_dict: dict, is_valid_contact_dict: dict) -> dict:
-    """
-    """
-    for key, value in primary_contacts_dict.items():
-        try:
-            is_valid = type(value) == dict
-        except: 
-            is_valid = False
-        is_valid_contact_dict[key] = is_valid_contact_dict[key] and is_valid
+    # for key, value in primary_contacts_dict.items():
+    #     is_valid = True
+    #     try:
+    #         is_valid = value["Name"].replace(" ", "").isalpha() and is_valid
+    #         for name in value["Name"].split(" "):
+    #             is_valid = name.replace(" ", "")[0].isupper() and is_valid
+    #     except: 
+    #         is_valid = True
+    #     is_valid_contact_dict[key] = is_valid_contact_dict[key] and is_valid
     return is_valid_contact_dict
 
 
@@ -191,24 +194,25 @@ if __name__ == '__main__':
     # Create id_contacts Dictionary
     id_contacts_dict = create_id_contacts_dict(df, args.primary_key, args.columns)
     # Create is_valid_contacts Dictionary
-    is_valid_contact_dict = {key: True for key, _ in id_contacts_dict.items()}
+    is_valid_contact_dict = {key: {"Number": True, "Email": True, "Extension": True, "Name": True} for key, _ in id_contacts_dict.items()}
 
     # Parse Contacts through OAI
     print("Calling OpenAI Fine-Tuned Model...")
     primary_contacts_dict = format_contacts_iteratively(id_contacts_dict)
+    for key, value in primary_contacts_dict.items():
+        print(f"{key}: {value}")
 
     # Test OAI Contacts 
-    print("\nTesting OpenAI Fine-Tuned Model responses...")
+    # print("\nTesting OpenAI Fine-Tuned Model responses...")
     validation_tests = [
         test_name_in_original_string,
         test_name_format,
-        test_is_dictionary
     ]
-    [test(id_contacts_dict, primary_contacts_dict, is_valid_contact_dict) for test in validation_tests]
+    # [test(id_contacts_dict, primary_contacts_dict, is_valid_contact_dict) for test in validation_tests]
 
     # PRINT TESTING RESULTS (CAN BE REMOVED LATER)
-    for key, value in is_valid_contact_dict.items():
-        print("\tID: " + str(key) + "\t\tResult: " + str(value))
+    # for key, value in is_valid_contact_dict.items():
+    #     print("\tID: " + str(key) + "\t\tResult: " + str(value))
 
     # Check Values Still Valid
     # valid_id_contacts_dict = filter_invalid_values(primary_contacts_dict, is_valid_contact_dict)
